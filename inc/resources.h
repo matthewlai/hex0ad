87ac5510593ea3b64105a75998a90f9223d4a5c9
Matthew Lai <m@matthewlai.ca>
2021-01-16 15:58:28 +0000

    Added skeleton parsing

--- inc/resources.h
+++ inc/resources.h
@@ -2,19 +2,20 @@
 #define RESOURCES_H
 
 static constexpr const char* kTestActorPaths[] = {
-    "structures/britons/civic_centre",
+    // "structures/britons/civic_centre",
 
-    "structures/mauryas/fortress",
-    "structures/britons/fortress",
-    "structures/persians/fortress",
-    "structures/romans/fortress",
-    "structures/spartans/fortress",
+    // "structures/mauryas/fortress",
+    // "structures/britons/fortress",
+    // "structures/persians/fortress",
+    // "structures/romans/fortress",
+    // "structures/spartans/fortress",
 
-    "structures/persians/stable",
-    "structures/persians/stable_elephant",
-    "units/athenians/hero_infantry_javelinist_iphicrates",
-    "units/romans/hero_cavalry_swordsman_maximus_r",
-    "units/romans/cavalry_javelinist_a_m",
+    // "structures/persians/stable",
+    // "structures/persians/stable_elephant",
+    // "units/athenians/hero_infantry_javelinist_iphicrates",
+    // "units/romans/hero_cavalry_swordsman_maximus_r",
+    // "units/romans/cavalry_javelinist_a_m",
+    "units/spartans/female_citizen",
     };
 
 static constexpr const char* kTestTerrainPaths[] = {
@@ -28,6 +29,10 @@ static constexpr const char* kTestTerrainPaths[] = {
     "biome-mediterranean/medit_sand_messy",
     };
 
+static constexpr const char* kSkeletonPaths[] = {
+    "biped",
+};
+
 static constexpr const char* kInputPrefix = "0ad_assets/";
 static constexpr const char* kOutputPrefix = "assets/";
 static constexpr const char* kActorPathPrefix = "art/actors/";
@@ -37,5 +42,7 @@ static constexpr const char* kActorTexturePathPrefix = "skins/";
 static constexpr const char* kVariantPathPrefix = "art/variants/";
 static constexpr const char* kTerrainPathPrefix = "art/terrains/";
 static constexpr const char* kTerrainTexturePathPrefix = "terrain/";
+
+static constexpr const char* kSkeletonPathPrefix = "art/skeletons/";
 
 #endif // RESOURCES_H
